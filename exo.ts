a31ffdb50dfc2d5c17c8faaf6ec532a16f55f919
sigmar <sigmarkarl@gmail.com>
2018-04-14 11:08:29 +0000

    added ts

--- exo.ts
+++ exo.ts
@@ -1,1 +1,15 @@
-alert("exo");
+function load() {
+    var exo = document.getElementById("exo");
+    var gl = exo.getContext("webgl");
+    if (!gl) {
+        document.querySelector("p").innerHTML =
+          "Failed to get WebGL context. "
+          + "Your browser or device may not support WebGL.";
+        return;
+    }
+    gl.viewport(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight);
+    gl.enable(gl.SCISSOR_TEST);
+    gl.scissor(30, 10, 60, 60);
+    gl.clearColor(1.0, 1.0, 0.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+}
